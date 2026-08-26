43eae8c17e209aaa4606bff132be63dae6e039a1
MrZongo-hash <aljoscha.novak@freenet.de>
2026-08-26 14:42:47 +0200

    Update mail_test.py

--- mail_test.py
+++ mail_test.py
@@ -4,47 +4,69 @@ from email.message import EmailMessage
 
 
 SMTP_SERVER = "mx.freenet.de"
-SMTP_PORT = 465
+SMTP_PORT = 587
 
 sender = os.environ["FREENET_EMAIL"]
 password = os.environ["FREENET_PASSWORD"]
 recipient = os.environ["MAIL_TO"]
 
 
-print("Verbinde mit Freenet SMTP über Port 465...")
+print("Verbinde mit Freenet SMTP...")
 
-with smtplib.SMTP_SSL(
+with smtplib.SMTP(
     SMTP_SERVER,
     SMTP_PORT,
     timeout=30
 ) as server:
 
-    print("SSL-Verbindung hergestellt.")
+    print("EHLO...")
+    print(server.ehlo())
+
+    print("STARTTLS...")
+    print(server.starttls())
+
+    print("EHLO nach TLS...")
+    print(server.ehlo())
 
     print("Login...")
-    server.login(sender, password)
+    print(server.login(sender, password))
 
     print("Login erfolgreich.")
 
-    msg = EmailMessage()
+    print("MAIL FROM...")
+    print(
+        server.mail(
+            sender,
+            options=["SMTPUTF8"]
+        )
+    )
 
+    print("RCPT TO...")
+    print(
+        server.rcpt(
+            recipient
+        )
+    )
+
+    print("DATA...")
+
+    msg = EmailMessage()
     msg["From"] = sender
     msg["To"] = recipient
-    msg["Subject"] = "STELLA Monitor – E-Mail-Test"
+    msg["Subject"] = "STELLA Monitor Test"
 
     msg.set_content(
-        "Hallo,\n\n"
-        "dies ist eine Test-E-Mail für den "
-        "STELLA-NRW-Monitor.\n\n"
-        "Wenn diese Nachricht angekommen ist, "
-        "funktioniert der E-Mail-Versand über Freenet.\n\n"
+        "Dies ist eine Testmail des "
+        "STELLA-NRW-Monitors.\n\n"
         "Viele Grüße\n"
         "STELLA Monitor"
     )
 
-    print("Sende Testmail...")
-
-    server.send_message(msg)
+    print(
+        server.data(
+            msg.as_bytes()
+        )
+    )
 
     print("E-Mail erfolgreich versendet.")
 
